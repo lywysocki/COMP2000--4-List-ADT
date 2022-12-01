cb8417ea09412eed1bbf97af2403bbc66ed27765
lywysocki <lywysocki@gmail.com>
2022-12-01 18:33:53 -0500

    genericPlayer

--- DS = 4 List App = Group nn (2022-3fa) = 2022-11-15 Tu 2138/src/edu/wit/scds/ds/list/app/GenericPlayer.java
+++ DS = 4 List App = Group nn (2022-3fa) = 2022-11-15 Tu 2138/src/edu/wit/scds/ds/list/app/GenericPlayer.java
@@ -1,16 +1,75 @@
 
-package edu.wit.scds.ds.list.app;
-
+package edu.wit.scds.ds.list.app ;
 
 /**
- * 
- * 
  * @author Laura Wysocki
+ * 
  * @version 1.0.0 2022-12-01 Initial implementation
- *
  */
-public class GenericPlayer
+public abstract class GenericPlayer extends Hand
     {
 
+    /**
+     * 
+     */
+    protected String name ;
+    protected boolean isBusted ;
+
+    /**
+     * @param name
+     */
+    public GenericPlayer( String name )
+        {
+        super() ; // we don't need to put it b/c hand already has it
+        this.name = name ;
+        isBusted = false ;
+
+        }
+
+
+    // getter for isBusted
+    /**
+     * 
+     * 
+     * @return
+     */
+    public boolean isBusted()
+        {
+        return isBusted ;
+
+        }
+
+
+    // setter of isBusted
+    /**
+     * 
+     * 
+     */
+    public void busted()
+        {
+        isBusted = true ;
+        System.out.printf( "%s: Busted!%n", name ) ;
+
+        }
+
+
+    @Override
+    public String toString()
+        {
+        String s ;
+        s = String.format( "%s: %s", name, super.toString() ) ;
+        return s ;
+
+        }
+
+
+    /**
+     * 
+     * 
+     * @param s
+     * @return
+     */
+    public abstract boolean isHitting( Scanner s ) ;
+
     }
-   // end class GenericPlayer
+// end class GenericPlayer
